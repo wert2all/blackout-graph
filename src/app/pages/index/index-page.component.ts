f7999e32950f58f662d95b795411f8ebc6eea9fc
Serhii Shvets <wert2all@gmail.com>
2024-06-27 14:31:24 +0300

    feat: add timeline component

--- src/app/pages/index/index-page.component.ts
+++ src/app/pages/index/index-page.component.ts
@@ -3,12 +3,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ContentComponent } from '../../share/layout/content/content.component';
 import { HeaderComponent } from '../../share/layout/header/header.component';
 import { LayoutComponent } from '../../share/layout/layout.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @Component({
   selector: 'app-index-page',
   templateUrl: './index-page.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [LayoutComponent, ContentComponent, HeaderComponent],
+  imports: [
+    LayoutComponent,
+    ContentComponent,
+    HeaderComponent,
+    TimelineComponent,
+  ],
 })
 export class IndexPageComponent {}
