b9093e4ae5d505c7dd8741f3d9ba26f3a133738e
wert2all <wert2all@gmail.com>
2024-06-27 18:54:10 +0300

    feat: add previous light items to timeline

--- src/app/app.types.ts
+++ src/app/app.types.ts
@@ -1,14 +1,16 @@
 export type WeekDay = 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
 export type GraphGroups = 'group3';
-
+export interface GraphLightItem {
+  time: string;
+  type: LightType;
+}
 export enum LightType {
   BLACKOUT = 'blackout',
   MAYBE_BLACKOUT = 'maybe-blackout',
   NORMAL = 'normal',
 }
 
-export interface LightItem {
-  time: string;
-  type: LightType;
-}
+export type LightItem = GraphLightItem & {
+  weekday: WeekDay;
+};
