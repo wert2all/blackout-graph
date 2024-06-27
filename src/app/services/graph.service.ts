e4501cf6240413968503130d42e7399e103224d1
wert2all <wert2all@gmail.com>
2024-06-27 19:01:00 +0300

    refactor: refactor graph service

--- src/app/services/graph.service.ts
+++ src/app/services/graph.service.ts
@@ -7,176 +7,39 @@ import {
   LightType,
   WeekDay,
 } from '../app.types';
-
-type Graph = Record<GraphGroups, Record<WeekDay, GraphLightItem[]>>;
+import { GraphStore } from './graph.store';
 
 @Injectable({ providedIn: 'root' })
 export class GraphService {
-  private readonly graph: Graph = {
-    group3: {
-      1: [
-        { time: '00:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '03:00', type: LightType.BLACKOUT },
-        { time: '04:00', type: LightType.BLACKOUT },
-        { time: '05:00', type: LightType.BLACKOUT },
-        { time: '06:00', type: LightType.BLACKOUT },
-        { time: '07:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '08:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '09:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '12:00', type: LightType.BLACKOUT },
-        { time: '13:00', type: LightType.BLACKOUT },
-        { time: '14:00', type: LightType.BLACKOUT },
-        { time: '15:00', type: LightType.BLACKOUT },
-        { time: '16:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '17:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '18:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '21:00', type: LightType.BLACKOUT },
-        { time: '22:00', type: LightType.BLACKOUT },
-        { time: '23:00', type: LightType.BLACKOUT },
-      ],
-      2: [
-        { time: '00:00', type: LightType.BLACKOUT },
-        { time: '01:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '02:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '03:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '06:00', type: LightType.BLACKOUT },
-        { time: '07:00', type: LightType.BLACKOUT },
-        { time: '08:00', type: LightType.BLACKOUT },
-        { time: '09:00', type: LightType.BLACKOUT },
-        { time: '10:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '11:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '12:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '15:00', type: LightType.BLACKOUT },
-        { time: '16:00', type: LightType.BLACKOUT },
-        { time: '17:00', type: LightType.BLACKOUT },
-        { time: '18:00', type: LightType.BLACKOUT },
-        { time: '19:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '20:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '21:00', type: LightType.MAYBE_BLACKOUT },
-      ],
-      3: [
-        { time: '00:00', type: LightType.BLACKOUT },
-        { time: '01:00', type: LightType.BLACKOUT },
-        { time: '02:00', type: LightType.BLACKOUT },
-        { time: '03:00', type: LightType.BLACKOUT },
-        { time: '04:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '05:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '06:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '09:00', type: LightType.BLACKOUT },
-        { time: '10:00', type: LightType.BLACKOUT },
-        { time: '11:00', type: LightType.BLACKOUT },
-        { time: '12:00', type: LightType.BLACKOUT },
-        { time: '13:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '14:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '15:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '18:00', type: LightType.BLACKOUT },
-        { time: '19:00', type: LightType.BLACKOUT },
-        { time: '20:00', type: LightType.BLACKOUT },
-        { time: '21:00', type: LightType.BLACKOUT },
-        { time: '22:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '23:00', type: LightType.MAYBE_BLACKOUT },
-      ],
-      4: [
-        { time: '00:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '03:00', type: LightType.BLACKOUT },
-        { time: '04:00', type: LightType.BLACKOUT },
-        { time: '05:00', type: LightType.BLACKOUT },
-        { time: '06:00', type: LightType.BLACKOUT },
-        { time: '07:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '08:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '09:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '12:00', type: LightType.BLACKOUT },
-        { time: '13:00', type: LightType.BLACKOUT },
-        { time: '14:00', type: LightType.BLACKOUT },
-        { time: '15:00', type: LightType.BLACKOUT },
-        { time: '16:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '17:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '18:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '21:00', type: LightType.BLACKOUT },
-        { time: '22:00', type: LightType.BLACKOUT },
-        { time: '23:00', type: LightType.BLACKOUT },
-      ],
-      5: [
-        { time: '00:00', type: LightType.BLACKOUT },
-        { time: '01:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '02:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '03:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '06:00', type: LightType.BLACKOUT },
-        { time: '07:00', type: LightType.BLACKOUT },
-        { time: '08:00', type: LightType.BLACKOUT },
-        { time: '09:00', type: LightType.BLACKOUT },
-        { time: '10:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '11:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '12:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '15:00', type: LightType.BLACKOUT },
-        { time: '16:00', type: LightType.BLACKOUT },
-        { time: '17:00', type: LightType.BLACKOUT },
-        { time: '18:00', type: LightType.BLACKOUT },
-        { time: '19:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '20:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '21:00', type: LightType.MAYBE_BLACKOUT },
-      ],
-      6: [
-        { time: '00:00', type: LightType.BLACKOUT },
-        { time: '01:00', type: LightType.BLACKOUT },
-        { time: '02:00', type: LightType.BLACKOUT },
-        { time: '03:00', type: LightType.BLACKOUT },
-        { time: '04:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '05:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '06:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '09:00', type: LightType.BLACKOUT },
-        { time: '10:00', type: LightType.BLACKOUT },
-        { time: '11:00', type: LightType.BLACKOUT },
-        { time: '12:00', type: LightType.BLACKOUT },
-        { time: '13:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '14:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '15:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '18:00', type: LightType.BLACKOUT },
-        { time: '19:00', type: LightType.BLACKOUT },
-        { time: '20:00', type: LightType.BLACKOUT },
-        { time: '21:00', type: LightType.BLACKOUT },
-        { time: '22:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '23:00', type: LightType.MAYBE_BLACKOUT },
-      ],
-      7: [
-        { time: '02:00', type: LightType.BLACKOUT },
-        { time: '03:00', type: LightType.BLACKOUT },
-        { time: '04:00', type: LightType.BLACKOUT },
-        { time: '05:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '08:00', type: LightType.BLACKOUT },
-        { time: '09:00', type: LightType.BLACKOUT },
-        { time: '10:00', type: LightType.BLACKOUT },
-        { time: '11:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '14:00', type: LightType.BLACKOUT },
-        { time: '15:00', type: LightType.BLACKOUT },
-        { time: '16:00', type: LightType.BLACKOUT },
-        { time: '17:00', type: LightType.MAYBE_BLACKOUT },
-        { time: '20:00', type: LightType.BLACKOUT },
-        { time: '21:00', type: LightType.BLACKOUT },
-        { time: '22:00', type: LightType.BLACKOUT },
-        { time: '23:00', type: LightType.MAYBE_BLACKOUT },
-      ],
-    },
-  };
+  private readonly graph = GraphStore;
+
   getLightItems(
     group: GraphGroups,
     weekday: WeekDay,
     hour: number,
   ): LightItem[] {
     const currentWeekDay = weekday;
-    const previousWeekDay = this.getPreviousWeekDay(weekday);
+    const previousWeekDay = this.getPreviousWeekDay(currentWeekDay);
+    const nextWeekDay = this.getNextWeekDay(currentWeekDay);
 
     const currentWeekdayItems = this.graph[group][weekday];
     const previousWeekdayItems = this.graph[group][previousWeekDay];
+    const newWeekdayItems = this.graph[group][nextWeekDay];
 
+    const currentItems = this.getListItems(currentWeekdayItems, currentWeekDay);
     const previousItems = this.getListItems(
       previousWeekdayItems,
       previousWeekDay,
     );
-    const currentItems = this.getListItems(currentWeekdayItems, currentWeekDay);
+    const nextItems = this.getListItems(newWeekdayItems, nextWeekDay);
 
-    return [...previousItems, ...currentItems];
+    return [...previousItems, ...currentItems, ...nextItems];
   }
+
+  private getNextWeekDay(weekDay: number): WeekDay {
+    return weekDay === 7 ? 1 : ((weekDay + 1) as WeekDay);
+  }
+
   private getPreviousWeekDay(weekday: WeekDay): WeekDay {
     return weekday === 1 ? 7 : ((weekday - 1) as WeekDay);
   }
