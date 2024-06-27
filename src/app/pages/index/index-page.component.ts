7556a68340e4125f48a7dcf935c9a804900e1808
Serhii Shvets <wert2all@gmail.com>
2024-06-27 13:26:28 +0300

    feat: add header

--- src/app/pages/index/index-page.component.ts
+++ src/app/pages/index/index-page.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { LayoutComponent } from '../../share/layout/layout.component';
+import { ContentComponent } from '../../share/layout/content/content.component';
+import { HeaderComponent } from '../../share/layout/header/header.component';
+
 @Component({
   selector: 'app-index-page',
   templateUrl: './index-page.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [LayoutComponent, ContentComponent, HeaderComponent],
 })
-export class IndexPageComponent {}
+export class IndexPageComponent { }
---
+++ src/app/share/layout/header/header.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent { }
