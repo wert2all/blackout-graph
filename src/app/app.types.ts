16b1e77667bd10ee1fb0b0fa6aa56893ff4596bd
Serhii Shvets <wert2all@gmail.com>
2024-06-30 15:31:19 +0300

    feat: add auto-refresh data

--- src/app/app.types.ts
+++ src/app/app.types.ts
@@ -1,6 +1,20 @@
+import { EffectConfig } from '@ngrx/effects';
+
 export type GraphGroups = 'group3';
 export const AppLocale = 'uk';
 
+export const StoreDispatchEffect: EffectConfig & {
+  functional: true;
+  dispatch?: true;
+} = { functional: true };
+
+export const StoreUnDispatchEffect: EffectConfig & {
+  functional: true;
+  dispatch: false;
+} = {
+  functional: true,
+  dispatch: false,
+};
 export const hourToString = (hour: number) =>
   hour < 10 ? `0${hour}:00` : `${hour}:00`;
 
--- src/app/pages/index/index-page.component.ts
+++ src/app/pages/index/index-page.component.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
+import { Duration } from 'luxon';
+import { interval } from 'rxjs';
 
 import { ContentComponent } from '../../share/layout/content/content.component';
 import { HeaderComponent } from '../../share/layout/header/header.component';
 import { LayoutComponent } from '../../share/layout/layout.component';
+import { WeekDayActions } from '../../store/graph.actions';
 import { graphFeature } from '../../store/graph.reducers';
 import { TimelineComponent } from './timeline/timeline.component';
 import { WeekGraphComponent } from './week-graph/week-graph.component';
 import { WeekdaysComponent } from './weekdays/weekdays.component';
-
 @Component({
   selector: 'app-index-page',
   templateUrl: './index-page.component.html',
@@ -24,5 +27,16 @@ import { WeekdaysComponent } from './weekdays/weekdays.component';
   ],
 })
 export class IndexPageComponent {
-  isWeekActive = inject(Store).selectSignal(graphFeature.selectIsWeek);
+  private readonly store = inject(Store);
+  private readonly timer$ = interval(
+    Duration.fromObject({ minute: 10 }).toMillis(),
+  ).pipe(takeUntilDestroyed());
+
+  isWeekActive = this.store.selectSignal(graphFeature.selectIsWeek);
+
+  constructor() {
+    this.timer$.subscribe(() => {
+      this.store.dispatch(WeekDayActions.autoRefresh());
+    });
+  }
 }
