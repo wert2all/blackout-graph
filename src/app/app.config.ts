533e1b2950d4da039183f28eac87726eabac5afb
wert2all <wert2all@gmail.com>
2024-07-21 18:52:26 +0300

    feat: add firebase support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,16 @@ import {
   isDevMode,
   provideZoneChangeDetection,
 } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
+import { EnvironmentType } from '../environments/environment.types';
 import { routes } from './app.routes';
 import { graphFeature } from './store/graph/graph.reducers';
 import { lightEffects } from './store/light/light.effects';
@@ -27,7 +31,7 @@ export const appConfig: ApplicationConfig = {
     provideEffects([lightEffects]),
     provideStoreDevtools({
       maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      logOnly: environment.type == EnvironmentType.production, // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
@@ -38,5 +42,7 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
 };
